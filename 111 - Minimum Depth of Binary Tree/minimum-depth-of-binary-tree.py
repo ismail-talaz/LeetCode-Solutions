bc724b4c46ec204a9a39490ee3d4dd4d90da4c64
ismail-talaz <sml0_tlz0@hotmail.com>
2023-07-10 18:27:34 +0300

    Delete minimum-depth-of-binary-tree.py

--- 111 - Minimum Depth of Binary Tree/minimum-depth-of-binary-tree.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None : return 0
-
-
-        if root.left is None and root.right is None:return 1
-        elif root.left is None: return self.minDepth(root.right)+1
-        elif root.right is None: return self.minDepth(root.left)+1
-        else: return min(self.minDepth(root.right)+1,self.minDepth(root.left)+1)
