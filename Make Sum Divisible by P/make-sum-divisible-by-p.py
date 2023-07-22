d917076d752785791fdd7199e43985ddc672772d
ismail-talaz <sml0_tlz0@hotmail.com>
2023-07-22 12:16:50 +0300

    Create make-sum-divisible-by-p.py

---
+++ Make Sum Divisible by P/make-sum-divisible-by-p.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        remainder=sum(nums)%p
+        if not remainder:return 0
+        mydict={0:-1}
+        curr=0
+        res=len(nums)
+        for i,num in enumerate(nums):
+            curr=(curr+num)%p
+            if (curr-remainder)%p in mydict:
+                res=min(res,i-mydict[(curr-remainder)%p])
+            mydict[curr]=i
+        
+        if res<len(nums):return res
+        else:
+            print(res)
+            return -1
