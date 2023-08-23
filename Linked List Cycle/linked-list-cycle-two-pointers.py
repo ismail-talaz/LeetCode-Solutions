99a0be1aefa64db2662b7159e336cccf06a3ecd2
ismail-talaz <sml0_tlz0@hotmail.com>
2023-08-23 23:04:29 +0300

    Update linked-list-cycle-two-pointers.py

--- Linked List Cycle/linked-list-cycle-two-pointers.py
+++ Linked List Cycle/linked-list-cycle-two-pointers.py
@@ -2,8 +2,8 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow,fast=head,head
         
-        while (fast!=None and fast.next!=None):
-            slow=slow.next
+        while (fast!=None and fast.next!=None):        # Time Complexity O(N)
+            slow=slow.next                             # Space Complexity O(1)
             fast=fast.next.next
             if (slow==fast):return True
         return False
