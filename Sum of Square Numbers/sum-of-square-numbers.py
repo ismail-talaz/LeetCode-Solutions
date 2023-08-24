d02024648a71cdbbc553e0e60041e9d38f6a9d41
ismail-talaz <sml0_tlz0@hotmail.com>
2023-08-24 11:19:01 +0300

    Update sum-of-square-numbers.py

--- Sum of Square Numbers/sum-of-square-numbers.py
+++ Sum of Square Numbers/sum-of-square-numbers.py
@@ -3,7 +3,7 @@ class Solution:
         x,y=0,int(math.sqrt(c))
 
         while (x<=y):
-            if (x*x+y*y)==c:return True
-            elif (x*x+y*y)<c:x+=1
+            if (x*x+y*y)==c:return True              # Time Complexity O(sqrt(c))
+            elif (x*x+y*y)<c:x+=1                    # Space Complexity O(1)
             else:y-=1
         return False
