19f080b3dab006c12bedde038719840fadbeec8b
ismail-talaz <sml0_tlz0@hotmail.com>
2023-08-25 09:19:33 +0300

    Update rotate-list.py

--- Rotate List/rotate-list.py
+++ Rotate List/rotate-list.py
@@ -6,8 +6,8 @@ class Solution:
         while (tail.next):
             length+=1
             tail=tail.next
-        k=(k%length)
-        if k==0:return head
+        k=(k%length)                        # Time Complexity O(N)
+        if k==0:return head                 # Space Complexity O(1)
         
         newTail=head
         for _ in range(length-k-1):
