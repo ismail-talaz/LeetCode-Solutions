32cdb87f8d17ec8f18db9c0646c78cb80888368b
ismail-talaz <sml0_tlz0@hotmail.com>
2023-08-29 22:19:43 +0300

    Update two-sum-II-input-array-is-sorted-binary-search.py

--- Two Sum II - Input Array Is Sorted/two-sum-II-input-array-is-sorted-binary-search.py
+++ Two Sum II - Input Array Is Sorted/two-sum-II-input-array-is-sorted-binary-search.py
@@ -1,11 +1,13 @@
-for i in xrange(len(numbers)):
-        l, r = i+1, len(numbers)-1
-        tmp = target - numbers[i]
-        while l <= r:
-            mid = l + (r-l)//2
-            if numbers[mid] == tmp:
-                return [i+1, mid+1]
-            elif numbers[mid] < tmp:
-                l = mid+1
-            else:
-                r = mid-1
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            left, right = i+1, len(numbers)-1
+            res = target - numbers[i]
+            while (left <= right):
+                mid = (left+right)//2             # Time Complexity O(nlogn)
+                if numbers[mid] == res:           # Space Complexity O(1)
+                    return [i+1, mid+1]
+                elif numbers[mid] < res:
+                    left = mid+1
+                else:
+                    right = mid-1
