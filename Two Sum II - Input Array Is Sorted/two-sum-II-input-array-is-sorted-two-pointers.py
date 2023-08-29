cff6e9e7d6a247d9a9bce128430a00336dcfd581
ismail-talaz <sml0_tlz0@hotmail.com>
2023-08-29 22:20:31 +0300

    Update two-sum-II-input-array-is-sorted-two-pointers.py

--- Two Sum II - Input Array Is Sorted/two-sum-II-input-array-is-sorted-two-pointers.py
+++ Two Sum II - Input Array Is Sorted/two-sum-II-input-array-is-sorted-two-pointers.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
         left,right=0,len(numbers)-1
-        while (left<right):                       # Two Pointers Approach
-            res=numbers[left]+numbers[right]
+        while (left<right):                       # Time Complexity O(N)
+            res=numbers[left]+numbers[right]      # Space Complexity O(1)
             if res>target:right-=1
             elif res<target:left+=1
             else:return [left+1,right+1]
