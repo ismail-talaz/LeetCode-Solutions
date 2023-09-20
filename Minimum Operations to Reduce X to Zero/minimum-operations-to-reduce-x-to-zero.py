65fc497cc9745e1ff4e2aefe0977a1de1037d721
ismail-talaz <sml0_tlz0@hotmail.com>
2023-09-20 20:44:32 +0300

    Create minimum-operations-to-reduce-x-to-zero.py

---
+++ Minimum Operations to Reduce X to Zero/minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        if sum(nums)<x:return -1
+        hasht={0:-1}
+        length=len(nums)
+
+        leftsum,rightsum=0,0
+        for i in range(length):
+            leftsum+=nums[i]
+            hasht[leftsum]=i
+            
+        result=float('inf')
+        
+        if x in hasht:result= hasht[x]+1
+
+        for i in range(length):
+            rightsum+=nums[length-i-1]
+            if x-rightsum in hasht:
+                result=min(i+hasht[x-rightsum]+2,result)
+        
+        if result==float('inf'):return -1
+        else: return result
